3bba139722cfc7ade537aecccfa3aef9eedd7a37
Joel Kusi <joelkkusi@gmail.com>
2025-08-22 01:19:24 +0200

    Added: terminal clearing, readability and custom input.

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-word = "apple"
-guesses = 5
+import os
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+clear()
+
+print("Welcome to the Guessing Game!")
+
+topic = input("Enter the topic of the word: ").lower()
+word = input("Enter the " + topic + " to guess: ").lower()
+guesses = int(input("Enter the number of guesses: "))
+
+clear()
 
 print("Guessing Game")
-print("Topic: Fruit")
-print("You'll start of with " + str(guesses) + " chances to guess the word.")
+print("Topic of the word: " + topic)
+print("You'll start of with " + str(guesses) + " chances to guess the " + topic + ".")
 
 for attempt in range(guesses):
-    guess = input("Guess the word: ")
+    guess = input("Guess the " + topic + ": ").lower()
 
     if guess == word:
-        print("Congratulations! You guessed the word!")
+        print("Congratulations! You guessed the " + topic + " correctly!")
         break
     else:
+        clear()
         guesses -= 1
         print("You have " + str(guesses) + " guesses left.")
 else:
-    print("Sorry, you ran out of guesses. The word was " + word)
+    clear()
+    print("Sorry, you ran out of guesses.\n"
+          "The " + topic + " was: " + word)
