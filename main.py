4bf2eb5984533bd6361f5d15e76172a0ff142207
Joel Kusi <joelkkusi@gmail.com>
2025-08-22 01:19:24 +0200

    Added: terminal clearing, readability and custom input.

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-word = "apple"
-guesses = 5
+import os
 
-print("Guessing Game")
-print("Topic: Fruit")
-print("You'll start of with " + str(guesses) + " chances to guess the word.")
 
-for attempt in range(guesses):
-    guess = input("Guess the word: ")
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    if guess == word:
-        print("Congratulations! You guessed the word!")
-        break
+
+clear()
+
+while True:
+    print("Welcome to the Guessing Game!")
+
+    topic = input("Enter the topic of the word: ").lower()
+    word = input("Enter the " + topic + " to guess: ").lower()
+
+    while True:
+        guess_validation = input("Enter the number of guesses: ").strip()
+        if guess_validation.isdigit() and int(guess_validation) > 0:
+            guesses = int(guess_validation)
+            break
+        print("Please enter a number.")
+
+    clear()
+
+    print("Guessing Game")
+    print("Topic of the word: " + topic)
+    print("You'll start of with " + str(guesses) + " chances to guess the " + topic + ".")
+
+    for attempt in range(guesses):
+        guess = input("Guess the " + topic + ": ").lower()
+
+        if guess == word:
+            print("Congratulations! You guessed the " + topic + " correctly!")
+            break
+        else:
+            clear()
+            guesses -= 1
+            print("You have " + str(guesses) + " guesses left to guess the " + topic + ".")
     else:
-        guesses -= 1
-        print("You have " + str(guesses) + " guesses left.")
-else:
-    print("Sorry, you ran out of guesses. The word was " + word)
+        clear()
+        print("Sorry, you ran out of guesses.\n"
+              "The " + topic + " was: " + word + "\n")
